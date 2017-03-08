00144f0699fbf11c5fc6fd5cd813449cdb3e013c
Chris Erwin <chris.erwin@ge.com>
2017-03-08 14:56:12 -0500

    abstract aws creds a bit more

--- example.tf
+++ example.tf
@@ -1,4 +1,8 @@
-provider "aws" {}
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region = "${var.region}"
+}
 
 resource "aws_vpc" "default" {
   cidr_block = "10.0.0.0/16"
