f2d7b309ec9f206e3daf6bda22aef25ddb1da9b8
Chris Erwin <chris.erwin@ge.com>
2017-03-09 15:57:12 -0500

    minor change to trigger apply

--- example.tf
+++ example.tf
@@ -1,11 +1,11 @@
 provider "aws" {
   access_key = "${var.access_key}"
   secret_key = "${var.secret_key}"
-  region = "${var.region}"
+  region     = "${var.region}"
 }
 
 resource "aws_vpc" "default" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
 
   tags {
@@ -22,14 +22,14 @@ resource "aws_internet_gateway" "default" {
 }
 
 resource "aws_route" "internet_access" {
-  route_table_id = "${aws_vpc.default.main_route_table_id}"
+  route_table_id         = "${aws_vpc.default.main_route_table_id}"
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id = "${aws_internet_gateway.default.id}"
+  gateway_id             = "${aws_internet_gateway.default.id}"
 }
 
 resource "aws_subnet" "default" {
-  vpc_id = "${aws_vpc.default.id}"
-  cidr_block = "10.0.1.0/24"
+  vpc_id                  = "${aws_vpc.default.id}"
+  cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
 
   tags {
@@ -38,21 +38,21 @@ resource "aws_subnet" "default" {
 }
 
 resource "aws_security_group" "default" {
-  name = "erwin_tf_sg"
+  name        = "erwin_tf_sg"
   description = "probe test - Erwin"
-  vpc_id = "${aws_vpc.default.id}"
+  vpc_id      = "${aws_vpc.default.id}"
 
   ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -62,14 +62,14 @@ resource "aws_security_group" "default" {
 }
 
 resource "aws_instance" "cerwintftest" {
-  ami           = "${lookup(var.amis, var.region)}"
-  instance_type = "t2.micro"
+  ami                    = "${lookup(var.amis, var.region)}"
+  instance_type          = "t2.micro"
   vpc_security_group_ids = ["${aws_security_group.default.id}"]
-  subnet_id = "${aws_subnet.default.id}"
-  key_name = "${lookup(var.key_pairs, var.region)}"
+  subnet_id              = "${aws_subnet.default.id}"
+  key_name               = "${lookup(var.key_pairs, var.region)}"
 
   tags {
-    Name = "probe test - Erwin"
-    Environment = "testing"
+    Name        = "probe test - Erwin"
+    Environment = "staging"
   }
 }
